e4d72ff3003709f6789a96c4ae002c9cca5b57e7
sonichy <sonichy@163.com>
2018-05-18 16:19:33 +0800

    add focus effect

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,6 +22,27 @@ MainWindow::MainWindow(QWidget *parent) :
     QTimer *timer = new QTimer(this);
     connect(timer,SIGNAL(timeout()),this,SLOT(updateTime()));
     timer->start(1000);
+
+    QMenu *menu = new QMenu;
+    menu->setAttribute(Qt::WA_TranslucentBackground);
+    menu->setAutoFillBackground(true);
+    QAction *action_logout = new QAction("注销",this);
+    QAction *action_reboot = new QAction("重启",this);
+    QAction *action_suspend = new QAction("待机",this);
+    QAction *action_hibernate = new QAction("休眠",this);
+    QAction *action_lock = new QAction("锁定",this);
+    menu->addAction(action_logout);
+    menu->addAction(action_reboot);
+    menu->addAction(action_suspend);
+    menu->addAction(action_hibernate);
+    menu->addAction(action_lock);
+    ui->pushButtonShutMenu->setMenu(menu);
+    connect(action_logout,SIGNAL(triggered()),this,SLOT(logout()));
+    connect(action_reboot,SIGNAL(triggered()),this,SLOT(reboot()));
+    connect(action_suspend,SIGNAL(triggered()),this,SLOT(suspend()));
+    connect(action_hibernate,SIGNAL(triggered()),this,SLOT(hibernate()));
+    connect(action_lock,SIGNAL(triggered()),this,SLOT(lock()));
+
     QString userName = QStandardPaths::standardLocations(QStandardPaths::HomeLocation).at(0).section('/', -1);
     ui->labelUserName->setText(userName);
     // 登录设置文件：/var/lib/AccountsService/users/用户名
@@ -41,15 +62,19 @@ MainWindow::MainWindow(QWidget *parent) :
     QString greeterBackground = settings->value("/User/GreeterBackground").toString().replace("file://","");
     // background-image 不能拉伸，border-image 可以自动拉伸。
     QString sstyle = "MainWindow { border-image:url(" + greeterBackground + ");}"
-                     "QMenu { border:1px solid rgba(255,255,255,10); background-color:rgba(255,255,255,10); }"
-                     "QMenu::item { border:1px solid rgba(255,255,255,10); background-color:rgba(255,255,255,10);}"
-                     "QMenu::item:selected { border:1px solid rgba(255,255,255,30); background-color:rgba(255,255,255,30); }"
+                     "QMenu { background:rgba(255,255,255,10); }"
+                     "QMenu::item { background:rgba(255,255,255,10);}"
+                     "QMenu::item:selected { background:rgba(255,255,255,30); }"
                      "QPushButton::menu-indicator { width:0px; }"
                      "QPushButton:focus { border:1px solid rgba(255,255,255,30); background-color:rgba(255,255,255,30); outline:none; }"
                      "QPushButton#pushButtonShutdown { border-image:url(:/shutdown.svg) }"
                      "QPushButton#pushButtonShutdown::hover { border-image:url(:/shutdown_hover.svg) }"
+                     "QPushButton#pushButtonShutdown::focus { border-image:url(:/shutdown_hover.svg) }"
                      "QPushButton#pushButtonShutMenu { border-image:url(:/menu.svg) }"
-                     "QPushButton#pushButtonShutMenu::hover { border-image:url(:/menu_hover.svg) }";
+                     "QPushButton#pushButtonShutMenu::hover { border-image:url(:/menu_hover.svg) }"
+                     "QPushButton#pushButtonShutMenu::focus { border-image:url(:/menu_hover.svg) }"
+                     "QLineEdit { background:rgba(255,255,255,10); border:5px solid rgba(255,255,255,10);}"
+                     "QLineEdit::focus { background:rgba(255,255,255,255); }";
     setStyleSheet(sstyle);
     ui->pushButtonShutdown->move(QApplication::desktop()->width() - 130, QApplication::desktop()->height() - 80);
     ui->pushButtonShutMenu->move(ui->pushButtonShutdown->x() + ui->pushButtonShutdown->width()+10, ui->pushButtonShutdown->y());
@@ -57,24 +82,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->verticalWidgetAccount->move((QApplication::desktop()->width()-ui->verticalWidgetAccount->width())/2, (QApplication::desktop()->height()-ui->verticalWidgetAccount->height())/2);
     connect(ui->lineEditPassword,SIGNAL(textChanged(QString)),this,SLOT(passwordChange(QString)));
     connect(ui->lineEditPassword,SIGNAL(returnPressed()),this,SLOT(on_pushButtonLogin_clicked()));
-
-    QMenu *menu = new QMenu;
-    QAction *action_logout = new QAction("注销",this);
-    QAction *action_reboot = new QAction("重启",this);
-    QAction *action_suspend = new QAction("待机",this);
-    QAction *action_hibernate = new QAction("休眠",this);
-    QAction *action_lock = new QAction("锁定",this);
-    menu->addAction(action_logout);
-    menu->addAction(action_reboot);
-    menu->addAction(action_suspend);
-    menu->addAction(action_hibernate);
-    menu->addAction(action_lock);
-    ui->pushButtonShutMenu->setMenu(menu);
-    connect(action_logout,SIGNAL(triggered()),this,SLOT(logout()));
-    connect(action_reboot,SIGNAL(triggered()),this,SLOT(reboot()));
-    connect(action_suspend,SIGNAL(triggered()),this,SLOT(suspend()));
-    connect(action_hibernate,SIGNAL(triggered()),this,SLOT(hibernate()));
-    connect(action_lock,SIGNAL(triggered()),this,SLOT(lock()));
 }
 
 MainWindow::~MainWindow()
